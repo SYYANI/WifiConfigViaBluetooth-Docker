8f9af52844f53126c1506112e7f6870fd7485385
SYYANI <wunongfeidao@live.com>
2021-10-30 04:43:40 +0800

    1.58

--- start-docker.sh
+++ start-docker.sh
@@ -1,10 +1,3 @@
-apt-get update &&
-curl -sSL https://get.docker.com | sh &&
-sudo usermod -aG docker pi &&
-sudo apt-get install -y libffi-dev libssl-dev &&
-sudo apt-get install -y python3 python3-pip &&
-sudo apt-get remove  -y python-configparser &&
-sudo pip3 -v install  docker-compose &&
 sudo systemctl disable bluetooth &&
 sudo systemctl stop bluetooth &&
 sudo systemctl mask bluetooth.service &&
@@ -18,17 +11,13 @@ sudo systemctl stop wpa_supplicant &&
 rfkill unblock all &&
 cd /home &&
 git clone https://gitee.com/syyan/aicare.git &&
-cd aicare &&
-docker build -t aicare/wifioverbt:1.0 . &&
-
-#docker-compose up -d
 
 docker run -d \
   --restart=always \
-  --name=aicare/wifi_over_bt \
+  --name=wifioverbt \
   --volume "/home/aicare/service:/etc/systemd/system/dbus-org.bluez.service:ro" \
   --volume "/home/aicare/start.sh:/start.sh" \
   --device "/dev/wlan0:/dev/wlan0" \
   --net=host \
   --privileged \
-  registry.cn-shanghai.aliyuncs.com/aicare/softether
+  registry.cn-shanghai.aliyuncs.com/aicare/wifi_over_bt:1.0
